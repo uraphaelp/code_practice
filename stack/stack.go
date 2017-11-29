ccec4a1cc59848bffaebe09d11d2579320e27de9
uraphaelp <uraphaelp@163.com>
2017-11-29 17:14:39 +0800

    Create stack.go

---
+++ stack/stack.go
@@ -0,0 +1,43 @@
+//定义链表
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+/定义栈：栈中的元素使用链表实现
+type Stack struct {
+    Head, Bottom *ListNode
+}
+
+//新增元素
+func (this *Stack) Push(x int)  {
+    pnew:=new(ListNode)
+    pnew.Val=x
+    pnew.Next=this.Head
+    this.Head=pnew
+}
+
+//弹出栈顶元素
+func (this *Stack) Pop()  {
+    this.Head=this.Head.Next    
+}
+
+//返回栈顶元素值
+func (this *Stack) Top() int {
+    return this.Head.Val
+}
+
+//取出栈顶中最小元素
+func (this *Stack) GetMin() int {
+    var min, tmp int
+    pnew:=this.Head
+    min=pnew.Val
+    for pnew!=nil {
+        tmp=pnew.Val
+        if tmp<min {
+            min=tmp
+        }
+        pnew=pnew.Next
+    }
+    return min
+}
